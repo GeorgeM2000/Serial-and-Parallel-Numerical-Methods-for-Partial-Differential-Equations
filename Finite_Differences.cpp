9e4fb285363b95121b49c2a49445027623a9b34e
GeorgeM2000 <giorgos_matlis@hotmail.com>
2023-04-15 15:33:37 +0300

    Message

--- Finite_Differences.cpp
+++ Finite_Differences.cpp
@@ -1,26 +1,3 @@
-/*
-The MIT License (MIT)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-
 class Finite_Differences {
     int N;
 
@@ -28,29 +5,33 @@ class Finite_Differences {
 
         Finite_Differences() {}
 
+        // Set the dimension of the linear system.
         void set_N(int n) {
-            N = n;
+            N = n - 2;
         }
 
+
+        // Create the tridiagonal matrix.
         template<typename T>
         double* create_tridiagonal_matrix(double h, T&& q, std::function<double(double)> p, std::vector<double> X_points) {
-            double* A = create_matrix(N-2, N-2);
+            double* A = create_matrix(N, N);
 
             h = h * h;
-            for(int i=0; i<N-2; i++) {
-                for(int j=0; j<N-2; j++) {
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
                     if(i == j) {
-                        A[i * (N-2) + j] = (2.0 * 1.0) + h*q(X_points[i+1]);
+                        A[i * N + j] = (2.0 * 1.0) + h*q(X_points[i+1]);
                     } else if(i == j-1 || i == j+1) {
-                        A[i * (N-2) + j] = -1.0 * 1.0;
+                        A[i * N + j] = -1.0 * 1.0;
                     } else {
-                        A[i * (N-2) + j] = 0.0;
+                        A[i * N + j] = 0.0;
                     }
                 }
             }
             return A;
         }
 
+        // Arange the the x points from a starting point to an end point by a step.
         std::vector<double> arange(double start, double end, double step) {
             std::vector<double> x_points;
             for(double s=start; s<end; s+=step) {
@@ -60,25 +41,27 @@ class Finite_Differences {
             return x_points;
         }
 
+        // Create the vector F(the right hand side of the linear system).
         template<typename T>
         double* create_vector_F(double h, T&& f, double* boundary_conditions, std::vector<double> X_points) {
-            double* F = create_vector(N-2);
+            double* F = create_vector(N);
             h = h * h;
 
             F[0] = h * f(X_points[1]) + boundary_conditions[0];
-            F[N-3] = h * f(X_points[N-2]) + boundary_conditions[1];
+            F[N-1] = h * f(X_points[N]) + boundary_conditions[1];
 
-            for(int i=1; i<N-3; i++){
+            for(int i=1; i<N-1; i++){
                 F[i] = h * f(X_points[i+1]);
             }
 
             return F;
         }
 
+
         template<typename T>
         double L2_NormError(T&& u, double h, double* X, std::vector<double> X_points) {
             double error = 0.0;
-            for(int i=0; i<N-2; i++) {
+            for(int i=0; i<N; i++) {
                 error += pow(fabs(u(X_points[i+1]) - X[i]), 2.0);
             }
             error = h * error;
@@ -88,15 +71,17 @@ class Finite_Differences {
 
         template<typename T>
         double Max_NormError(T&& u, double h, double* X, std::vector<double> X_points) {
-            double local_Error[N-2];
+            double local_Error[N];
             for(int i=0; i<N-2; i++) {
                 local_Error[i] = fabs(X[i] - u(X_points[i+1]));
             }
-            double* max = std::max_element(local_Error, local_Error+(N-2));
+            double* max = std::max_element(local_Error, local_Error+N);
 
             return *max;
         }
 
+
+        // Compare the approximate solution with the exact solution.
         template<typename T>
         void compare_solutions(T&& u, double* X, std::vector<double> x_points, int limit) {
             std::cout.precision(std::numeric_limits<double>::max_digits10);
--- Schwarz_DD.cpp
+++ Schwarz_DD.cpp
@@ -1,25 +1,3 @@
-/*
-The MIT License (MIT)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 class Schwarz_DD {
 
     Jacobi j;
@@ -34,6 +12,8 @@ class Schwarz_DD {
             gs = gauss_seidel; 
         }
 
+
+        // Algebraic Block Jacobi Additive Schwarz Method
         int ABJASM(double *X,
                     double TOL,
                     int max_iterations,
@@ -106,7 +86,7 @@ class Schwarz_DD {
             }
 
 
-
+            // Matrix Block Jacobi Additive Schwarz Method
             int MBJASM(
                     double *X,
                     double TOL,
@@ -175,7 +155,7 @@ class Schwarz_DD {
             }
 
 
-
+            // Matrix Block Jacobi Restricted Additive Schwarz Method
             int MBJRASM(
                     double *X,
                     double TOL,
@@ -256,6 +236,7 @@ class Schwarz_DD {
 
             }
 
+            // Algebraic Additive Schwarz residual calculation.
             double* AAS_Residual_Calc(double* INV, double* F, int N, int vector_begin_index, int W, int subdomain_index, double residual_component) {
                 double *V = create_vector(N);
                 initialize_vector(V, 0.0, N);
